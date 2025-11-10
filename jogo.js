1ae66a915d3519ccf46fda0db939ba9313776f6c
vitoriaemcosta-gif <vitoriae.m.costa@gmail.com>
2025-11-10 16:30:36 -0300

    JOGO

--- jogo.js
+++ jogo.js
@@ -17,16 +17,48 @@ var caixa_amarelo = imagem('imagens/3.png', 310, 30);
 var caixa_verde = imagem('imagens/2.png', 455, -15);
 
 
+
+
 function imagem2(url, x, y) {
   var img2 = new Image();
   img2.src = url;
   img2.onload = function () {
-    ctx.drawImage(img2, x, y, 600, 480);
+    ctx.drawImage(img2, x, y, 800, 710);
   };
   return img2;
 }
 
 // Chamando a função para cada imagem com sua posição
-var esteira = imagem2('imagens/esteira.png', 0, 200);
+var esteira = imagem2('imagens/esteira.png', 40, 82);
 
 
+ // Lista dos frames (substitua pelos caminhos corretos das imagens)
+ const frames = [
+  "imagens/lixeirafogo_frame_dif_1.png",
+  // "imagens/lixeirafogo_frame_dif_5.png",
+  "imagens/lixeirafogo_frame_dif_10.png"
+];
+
+const images = [];
+let currentFrame = 0;
+
+// Carregar as imagens
+frames.forEach((src, i) => {
+  const img = new Image();
+  img.src = src;
+  images.push(img);
+});
+
+function animate() {
+  const img = images[currentFrame];
+  if (img.complete) {
+    ctx.drawImage(img, 650, 170, 170, 220);
+  }
+  currentFrame = (currentFrame + 1) % images.length;
+  setTimeout(animate, 300); // muda a velocidade da animação
+}
+
+// Inicia a animação quando as imagens estiverem carregadas
+window.onload = () => {
+  animate();
+};
